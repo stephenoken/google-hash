3781c3a35c1cfb35f6d936c8a71579e66c91ced4
Stephen O'Kennedy <stephenoken@gmail.com>
2016-02-11 18:51:51 +0000

    "Added drone class"
    ""

--- index.rb
+++ index.rb
@@ -1,7 +1,21 @@
-
-class bootstrap
+class Bootstrap
   def initialize
     textFile = IO.readlines("example.in")
-    puts textFile
+    scenarioConfig = textFile[0].split
+    $table = {
+      :rows => scenarioConfig[0],
+      :cols => scenarioConfig[1],
+      :drones => scenarioConfig[2],
+      :turns => scenarioConfig[3],
+      :max_payload => scenarioConfig[4]
+    }
+    puts $table
   end
 end
+
+class Grid
+  def initialize()
+
+  end
+end
+bootstrap = Bootstrap.new
